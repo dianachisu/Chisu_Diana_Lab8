690d21ee3083a39b1f96733ef1ebc9bf9cf27a2a
Diana <Diana@DESKTOP-AR2QDPU>
2020-11-27 14:34:00 +0200

    Add project files.

---
+++ Data/Chisu_Diana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chisu_Diana_Lab8.Models;
+
+namespace Chisu_Diana_Lab8.Data
+{
+    public class Chisu_Diana_Lab8Context : DbContext
+    {
+        public Chisu_Diana_Lab8Context (DbContextOptions<Chisu_Diana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chisu_Diana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Chisu_Diana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Chisu_Diana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Chisu_Diana_Lab8.Data;
+using Chisu_Diana_Lab8.Models;
+
+namespace Chisu_Diana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Chisu_Diana_Lab8.Data.Chisu_Diana_Lab8Context _context;
+
+        public IndexModel(Chisu_Diana_Lab8.Data.Chisu_Diana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
